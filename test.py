7596628991912db63ca5bc225ceda49b308b8aba
ergl <ergl@users.noreply.github.com>
2015-07-06 13:53:21 +0200

    Added sample streaming use
    Updated gitignore

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-import auth_tokens
+#!/usr/bin/env python3
+
+from auth_tokens import *
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_stream = TwitterStream(auth=oauth)
+
+iterator = twitter_stream.statuses.sample()
+
+tweet_count = 1
+for tweet in iterator:
+    tweet_count -= 1
+    print(json.dumps(tweet, indent=4))
+    if tweet_count <= 0:
+        break
