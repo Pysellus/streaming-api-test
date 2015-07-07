9eaa61b8f9481842cd77783b0df803b4df1c2dd8
ergl <ergl@users.noreply.github.com>
2015-07-07 17:52:56 +0200

    Removed unnecessary json import on APIReader

--- APIReaderTwitter.py
+++ APIReaderTwitter.py
@@ -3,11 +3,6 @@
 from auth_tokens import *
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
 def get_iterable():
 	oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 	twitter_stream = TwitterStream(auth=oauth)
