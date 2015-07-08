5056278f481fb46bfb3db929c8351728990d0a49
ergl <ergl@users.noreply.github.com>
2015-07-08 13:46:19 +0200

    Added example of subscribe blocking test execution

---
+++ rx-tests/rxpy-observable-blocking.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+'''
+    Subscribing to an Observable is a blocking operation
+'''
+
+from rx import Observable
+import APIReaderTwitter as Twitter
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Iterable -> Observable
+def Stream(iterable):
+    return Observable.from_(iterable)
+
+# Any -> Boolean
+def is_not_delete(element):
+    return not "delete" in element
+
+def pretty_print(element):
+    print(json.dumps(element, indent=4))
+
+stream = Stream(Twitter.get_iterable())
+stream.filter(is_not_delete).subscribe(pretty_print)
+
+# This line never gets executed
+while True:
+    print("Foo")
