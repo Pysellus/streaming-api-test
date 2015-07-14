bd4633c19d80f5f4faf2c78606f5012e3392e7b5
Ángel Sanz <angelsanzgit@gmail.com>
2015-07-14 16:16:14 +0200

    Minor refactor.

--- telegram-discrete-streamification/Telegram.py
+++ telegram-discrete-streamification/Telegram.py
@@ -1,6 +1,6 @@
 """
     Telegram API - Provides an iterable
-    
+
     Keeps an internal buffer of updates.
     On next, get the next element from the buffer
 
@@ -25,20 +25,19 @@ class Telegram():
     def __next__(self):
         """
         Yield the next element from the buffer.
-        
+
         If no element is found, fill the buffer,
         and then return the first from it
         """
-        if self.buffer:
-            return self.buffer.pop(0)
-        else:
+        if not self.buffer:
             self._fill_update_buffer()
-            return self.buffer.pop(0)
+
+        return self.buffer.pop(0)
 
     def _fill_update_buffer(self, backoff=0.1, limit=16):
         """
         Fill the buffer with the next N elements.
-        
+
         If no connection can be made, keep trying
         If the buffer is still empty, keep trying
         """
@@ -63,6 +62,6 @@ class Telegram():
             self._fill_update_buffer(backoff=backoff*2)
         else:
             self.offset = res[-1]['update_id'] + 1
-        
+
         for up in res:
             self.buffer.append(up)
